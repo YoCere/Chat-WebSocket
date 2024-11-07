28d899a710f4b4cfae4e95e792e5e12cc250cc8c
YoCere <jcerezorios@gmail.com>
2024-11-07 12:02:45 -0400

    Creado archivo html con puerto del servidor configurado

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const path = require('path');
 const express = require('express');
 const app=express();
 
 app.set('port', process.env.PORT||3000);
+
+//static
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), ()=>{
     console.log('server on port', app.get('port'));
